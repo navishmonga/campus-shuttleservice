170f4df1694f5b2ad2536d3f2b744f93321b6cb2
navishmonga <141874091+navishmonga@users.noreply.github.com>
2023-08-24 22:54:12 +0530

    second commit

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import '../styles/Content.css';
+import React, { useState, useEffect } from 'react';
 
 function Notifications() {
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    //Space for API call
+    setTimeout(() => {
+      const sampleNotifications = [
+        { id: 1, message: 'Shuttle #23 is arriving in 5 minutes.' },
+        { id: 2, message: 'Shuttle #45 has been delayed due to maintenance.' },
+        { id: 3, message: 'New shuttle timings have been updated.' },
+      ];
+      setNotifications(sampleNotifications);
+    }, 1000);
+  }, []);
+
   return (
-    <div className="content">
+    <div className="notifications">
       <h2>Notifications</h2>
-      {/* Display user notifications */}
+      {notifications.length === 0 ? (
+        <p>Loading notifications...</p>
+      ) : (
+        <ul>
+          {notifications.map((notification) => (
+            <li key={notification.id}>{notification.message}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
